424707bd7da47a64b8b252f570cd13ca88a08580
Michael Perel <miperel@Michaels-MBP.guest.corp.microsoft.com>
2018-01-30 16:29:39 +0200

    Project Detail Screen finished

--- src/ExistingProjectsScreen/ProjectDetailScreen.js
+++ src/ExistingProjectsScreen/ProjectDetailScreen.js
@@ -1,27 +1,62 @@
 import React from "react";
-import { Container, Header, Title, Left, Icon, Right, Button, Body, Content, Text} from "native-base";
-import { StackNavigator} from 'react-navigation';
-export default class HomeScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => ({
-        header: (
-          <Header>
-            <Left>
-              <Button transparent onPress={() => navigation.goBack()}>
-                <Icon name="arrow-back" />
-              </Button>
-            </Left>
-            <Body>
-              <Title>Single Project</Title>
-            </Body>
-            <Right />
-          </Header>
-        )
-      });
+import { Image } from 'react-native';
+import { Container, Header, Title, Left, Icon, Right, Button, Body, Content, Text, View } from "native-base";
+import { StackNavigator } from 'react-navigation';
+export default class ProjectDetailScreen extends React.Component {
 
-      render() {
-        const { state, navigate } = this.props.navigation;
+    static navigationOptions = {
+        header: null
+    }
+
+    _renderImage() {
         return (
-            <Text>{JSON.stringify(state.params)}</Text>
+            <View style={{ flex: 1, flexDirection: "row" }}>
+                <Image source={{ uri: 'https://cdn.xl.thumbs.canstockphoto.com/example-blue-square-stamp-isolated-on-white-background-clip-art_csp23367728.jpg' }}
+                    style={{ height: 200, flex: 1 }} resizeMode='cover' />
+            </View>
+        );
+    }
+
+    _renderText = () => {
+        const { state } = this.props.navigation;
+        let projectData = state.params;
+        let textViews = Object.keys(projectData).map((key, index) => {
+            let legibleKey = (key.charAt(0).toUpperCase() + key.slice(1)).replace(".", " ");
+            return (
+                <View padder style={{ flex: 1, flexDirection: "row" }}
+                    key={index}>
+                    <Text style={{ fontWeight: "bold", flex: 1 }}>{legibleKey}</Text>
+                    <Text style={{ flex: 3 }}>{projectData[key]}</Text>
+                </View>
+            );
+        });
+
+        return (
+            <View>{textViews}</View>
+        );
+    }
+
+    render() {
+        const { state } = this.props.navigation;
+        let projectData = state.params;
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent onPress={() => this.props.navigation.goBack()}>
+                            <Icon name="arrow-back" />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>{projectData.name}</Title>
+                    </Body>
+                    <Right />
+                </Header>
+                <Content>
+                    {this._renderImage()}
+                    {this._renderText()}
+                </Content>
+            </Container>
         );
     }
 }
