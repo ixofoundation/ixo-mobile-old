929861c15428ca52f668fbb51148bb6f17b27684
Michael Perel <miperel@Michaels-MBP.guest.corp.microsoft.com>
2018-01-31 16:51:28 +0200

    flattened data

--- src/ExistingProjectsScreen/ProjectListScreen.js
+++ src/ExistingProjectsScreen/ProjectListScreen.js
@@ -12,16 +12,37 @@ export default class HomeScreen extends React.Component {
         header: null
     }
 
+    constructor(props) {
+        super(props);
+        this.flatData = ExistingProjectsData.map((value, index) => {
+            return this._flattenData(value, [], {});
+        })
+    }
+
+    _flattenData = (data, accum, flatData) => {
+        for (key in data) {
+            accum.push(key);
+            if (data[key] !== null && typeof data[key] === "object") {
+                this._flattenData(data[key], accum, flatData);
+            } else {
+                let flatString = accum.join(".");
+                accum.pop()
+                flatData[flatString] = data[key];
+            }
+        }
+        return flatData
+    }
+
     _renderRow = (item, _, index) => {
         return (
             <ListItem
                 item={item}
                 index={index}
-                onPress={() => this.props.navigation.navigate("ProjectDetailScreen", ExistingProjectsData[index])}>
+                onPress={() => this.props.navigation.navigate("ProjectDetailScreen", this.flatData[index])}>
                 <Thumbnail square size={80} source={{ uri: 'https://cdn.xl.thumbs.canstockphoto.com/example-blue-square-stamp-isolated-on-white-background-clip-art_csp23367728.jpg' }} />
                 <Body>
-                    <Text>{ExistingProjectsData[index]['name']}</Text>
-                    <Text note>{ExistingProjectsData[index]['country']} | {ExistingProjectsData[index]['owner.name']}</Text>
+                    <Text>{this.flatData[index]['name']}</Text>
+                    <Text note>{this.flatData[index]['country']} | {this.flatData[index]['owner.name']}</Text>
                 </Body>
             </ListItem>
         );
@@ -44,7 +65,7 @@ export default class HomeScreen extends React.Component {
                     <Right />
                 </Header>
                 <Content padder>
-                    <List dataArray={ExistingProjectsData}
+                    <List dataArray={this.flatData}
                         renderRow={this._renderRow}>
                     </List>
                 </Content>
